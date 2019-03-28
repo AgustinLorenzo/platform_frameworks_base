b1c35291843beabc7a6a43bf7c44a4111e668ded
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    @AgustinLorenzo implemented in AOSiP framework, backported from Havoc-OS

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5279,6 +5279,16 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
